2bcb7744d2de5776bd8d44422626bfe6b8f007c2
Jordy Greenblatt <jordan.s.greenblatt@gmail.com>
2024-11-07 08:51:29 -0500

    Create single endpoint to handle AI and icon APIs
    This removes the need for the user to enter their
    keys in the API.

--- flags.js
+++ flags.js
@@ -1,18 +1,18 @@
-
+require('dotenv').config();
 const { program } = require('commander');
 
 program
     .option('-npk --noun_project_key <noun_project_key>', 'Key for Noun Project API')
     .option('-nps --noun_project_secret <noun_project_secret>', 'Secret for Noun Project API')
+    .option('-gk --gemini_key', "Key for Google's Gemini API")
     .option('-p, --port <port>', 'Port to listen on');
     
 program.parse(process.argv);
 const options = program.opts();
 
-console.log(JSON.stringify(process.env, null, 2));
-
-const port = options.port ?? 3000;
 const noun_project_key = options.noun_project_key ?? process.env.NOUN_PROJECT_KEY;
 const noun_project_secret = options.noun_project_secret ?? process.env.NOUN_PROJECT_SECRET;
+const gemini_key = options.gemini_key ?? process.env.GEMINI_KEY;
+const port = options.port ?? 3000;
 
-module.exports = {port, noun_project_key, noun_project_secret};
+module.exports = {port, noun_project_key, noun_project_secret, gemini_key};
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-const iconRoute = require('./icon-route');
+const themeRoute = require('./theme-route');
 const { port } = require('./flags');
 
 const app = express();
 app.use(cors());
 
-app.use('/icon', iconRoute);
+app.use('/theme', themeRoute);
 
 app.listen(port, () => {
   console.log(`Themeleon server listening on port ${port}`);
